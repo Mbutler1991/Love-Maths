7a76e7615fad7019aa833ec5090abcc47ccb8a51
Mbutler1991 <markbutlermb1991@gmail.com>
2023-11-16 13:48:18 +0000

    added subtraction game functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,7 +27,9 @@ function runGame(gameType) {
         displayAdditionQuestion(num1, num2);
     } else if (gameType === "multiply") {
         displayMultiplyQuestion(num1, num2);
-    }else {
+    } else if (gameType ==="subtract") {
+        displaySubtractQuestion(num1, num2);
+    } else {
         alert(`unknown game type: ${gameType}`);
         throw `unknown game type: ${gameType}. Aborting!`;
     }
@@ -59,7 +61,9 @@ function calculateCorrectAnswer() {
         return [operand1 + operand2, 'addition'];
     } else if (operator === 'x') {
         return [operand1 * operand2, 'multiply'];
-    }else {
+    } else if (operator === '-') {
+        return [operand1 - operand2, 'subtract'];
+    } else {
         alert(`unimplemented operator ${operator}`);
         throw `unimplemented operator ${operator}.Aborting!`;
     }
@@ -84,8 +88,10 @@ function displayAdditionQuestion(operand1, operand2) {
     document.getElementById(`operator`).textContent = "+";
 }
 
-function displaySubtractQuestion() {
-
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById(`operand1`).textContent = operand1 > operand2 ? operand1 : operand2;
+    document.getElementById(`operand2`).textContent = operand1 > operand2 ? operand2 : operand1;
+    document.getElementById(`operator`).textContent = "-";
 }
 
 function displayMultiplyQuestion(operand1, operand2) {
